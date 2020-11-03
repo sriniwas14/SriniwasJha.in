83dde26ec25b51456d81bba58238983bb0fbc88b
sriniwas14 <sriniwasx@gmail.com>
2020-11-03 17:15:18 +0530

    Added Slides data in ApiCalls file

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -4,10 +4,36 @@ exports.activeProjects = [
     { projectId: "3", title: "Project Three", description: "Lorem Ipsum dolor sit amet", badges: ["Web App", "ReactJs", "NodeJs"], src: "https://via.placeholder.com/350x200" },
 ]
 
+exports.slides = [
+    { projectId: "1", slides:[
+        { title: "First", description: "First", image: "https://via.placeholder.com/1200x400" },
+        { title: "Second", description: "Second", image: "https://via.placeholder.com/1200x400" },
+        { title: "Third", description: "Third", image: "https://via.placeholder.com/1200x400" }
+    ] },
+    { projectId: "2", slides:[
+        { title: "First", description: "First", image: "https://via.placeholder.com/1200x400" },
+        { title: "Second", description: "Second", image: "https://via.placeholder.com/1200x400" },
+        { title: "Third", description: "Third", image: "https://via.placeholder.com/1200x400" }
+    ] },
+    { projectId: "3", slides:[
+        { title: "First", description: "First", image: "https://via.placeholder.com/1200x400" },
+        { title: "Second", description: "Second", image: "https://via.placeholder.com/1200x400" },
+        { title: "Third", description: "Third", image: "https://via.placeholder.com/1200x400" }
+    ] }
+]
+
 exports.getProjectById = (id) => {
     for(var i=0; i<this.activeProjects.length; i++){
         if(id===this.activeProjects[i].projectId){
             return this.activeProjects[i]
+        }
+    }
+}
+
+exports.getSlidesById = (id) => {
+    for(var i=0; i<this.slides.length; i++){
+        if(id===this.slides[i].projectId){
+            return this.slides[i]
         }
     }
 }

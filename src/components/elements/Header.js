0a49d576d4f69979e8295daabc3bb4714a83f770
sriniwas14 <sriniwasx@gmail.com>
2020-11-08 13:43:01 +0530

    Switched to a dark theme

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -3,7 +3,7 @@ import { Navbar, Nav } from 'react-bootstrap'
 
 export default function Header() {
     return (
-        <Navbar bg="custom-primary" variant="dark">
+        <Navbar variant="dark">
             <Nav className="ml-auto">
                 <Nav.Link active href="#home">Home</Nav.Link>
                 <Nav.Link href="#features">Projects</Nav.Link>

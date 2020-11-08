81367c56307aa0c54e1869769b8ddc952434117d
sriniwas14 <sriniwasx@gmail.com>
2020-11-08 11:31:06 +0530

    Added Project Details component in ProjectSingle

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,7 +1,7 @@
 exports.activeProjects = [
-    { projectId: "1", title: "Project One", description: "Lorem Ipsum dolor sit amet", badges: ["Video Game", "Godot", "GDScript"], src: "https://via.placeholder.com/350x200" },
-    { projectId: "2", title: "Project Two", description: "Lorem Ipsum dolor sit amet", badges: ["Desktop App", "Java"], src: "https://via.placeholder.com/350x200" },
-    { projectId: "3", title: "Project Three", description: "Lorem Ipsum dolor sit amet", badges: ["Web App", "ReactJs", "NodeJs"], src: "https://via.placeholder.com/350x200" },
+    { projectId: "1", title: "Project One", description: "Lorem Ipsum dolor sit amet", introduction: "Lorem Ipsum dolor sit amet", badges: ["Video Game", "Godot", "GDScript"], src: "https://via.placeholder.com/350x200", details:{ platforms: ["Android", "Windows"], status: "In Development", projectUrl: "https://www.github.com", releaseDate: "TBD" } },
+    { projectId: "2", title: "Project Two", description: "Lorem Ipsum dolor sit amet", introduction: "Lorem Ipsum dolor sit amet", badges: ["Desktop App", "Java"], src: "https://via.placeholder.com/350x200", details:{ platforms: ["Android", "Windows"], status: "In Development", projectUrl: "https://www.github.com", releaseDate: "TBD" } },
+    { projectId: "3", title: "Project Three", description: "Lorem Ipsum dolor sit amet", introduction: "Lorem Ipsum dolor sit amet", badges: ["Web App", "ReactJs", "NodeJs"], src: "https://via.placeholder.com/350x200", details:{ platforms: ["Android", "Windows"], status: "In Development", projectUrl: "https://www.github.com", releaseDate: "TBD" } },
 ]
 
 exports.slides = [
--- src/components/ProjectSingle.js
+++ src/components/ProjectSingle.js
@@ -6,20 +6,21 @@ import { AiOutlineLeft, AiOutlineGithub  } from 'react-icons/ai'
 
 import { getProjectById, getSlidesById } from '../ApiCalls'
 import CustomCarousel from './elements/CustomCarousel'
+import ProjectSingleDetails from './elements/ProjectSingleDetails'
 
 export default function ProjectSingle() {
     let { projectId } = useParams();
-    const [project, setProject] = useState({ title: "Hola" });
+    const [project, setProject] = useState({ title: "", details: [] });
     const [slides, setSlides] = useState([])
     
     useEffect(() => {
         setSlides(getSlidesById(projectId).slides)
-    })
+    }, [])
 
 
     useEffect(() => {
         setProject(getProjectById(projectId))
-    })
+    }, [])
 
     return (
         <Container>
@@ -34,11 +35,24 @@ export default function ProjectSingle() {
                                 <h5 style={{ textAlign: "center", margin: 0 }}>{project.title}</h5>
                             </div>
                             <div style={{ textAlign: "right" }}>
-                                <a href={ project.github } target="_blank"><AiOutlineGithub fontSize={35}/></a>
+                                <a href={ project.details.projectUrl } target="_blank" rel="noreferrer"><AiOutlineGithub fontSize={35}/></a>
                             </div>
                         </div>
                     )}/>
                     <CustomCarousel slides={slides} />
+
+                    <TextSection variant="dark" render={(
+                        <Row>
+                            <Col sm={6}>
+                                <h4>Introduction</h4>
+                                <p>{ project.introduction }</p>
+                            </Col>
+                            <Col sm={6}>
+                                <h4>Details</h4>
+                                <ProjectSingleDetails details={ project.details }/>
+                            </Col>
+                        </Row>
+                    )} />
                 </Col>
             </Row>
         </Container>

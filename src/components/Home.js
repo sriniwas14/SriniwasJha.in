108389cf5a8a8dc23c7eeb37dde44475c5fd67a5
sriniwas14 <sriniwasx@gmail.com>
2020-11-08 16:29:01 +0530

    Added Loader

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ import TextSection from './elements/TextSection'
 import ProjectScroller from './elements/ProjectScroller'
 import { activeProjects } from '../ApiCalls'
 
+import { AiOutlineLinkedin, AiOutlineGithub, AiOutlineTwitter  } from 'react-icons/ai'
+
 export default function Home() {
     return (
         <Container>
@@ -14,7 +16,26 @@ export default function Home() {
             </Row>
             <Row>
                 <Col sm={12}>
-                    <TextSection variant="dark" heading="Active Projects" render={(<ProjectScroller projects={activeProjects} />)} />
+                    <TextSection variant="dark" heading="Programming" render={(<ProjectScroller projects={activeProjects} />)} />
+                </Col>
+            </Row>
+            <Row>
+                <Col sm={12}>
+                    <TextSection heading="3D Models" render={(<ProjectScroller projects={activeProjects} />)} />
+                </Col>
+            </Row>
+            <Row>
+                <Col sm={12}>
+                    <TextSection variant="dark" render={(<Row>
+                        <Col sm={6} style={{ fontSize: "17px", paddingTop: "3px", cursor: "default" }}>
+                            <p>Copyright © Sriniwas Jha</p>
+                        </Col>
+                        <Col sm={6} style={{ textAlign: "right" }}>
+                            <a className="footerLinks" href="https://www.linkedin.com/in/sriniwasj/" target="_black" rel="noreferrer"><AiOutlineLinkedin fontSize={30} /></a>
+                            <a className="footerLinks" href="https://github.com/sriniwas14" target="_black" rel="noreferrer"><AiOutlineGithub fontSize={30} /></a>
+                            <a className="footerLinks" href="https://twitter.com/Sriniwasj" target="_black" rel="noreferrer"><AiOutlineTwitter fontSize={30} /></a>
+                        </Col>
+                    </Row>)} />
                 </Col>
             </Row>
         </Container>

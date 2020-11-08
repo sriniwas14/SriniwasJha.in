af228c9ffee56979d911ef8485310dbf29cead44
sriniwas14 <sriniwasx@gmail.com>
2020-11-08 11:31:06 +0530

    Added Project Details component in ProjectSingle
    	modified:   src/ApiCalls.js
    	modified:   src/components/ProjectSingle.js
    	new file:   src/components/elements/ProjectSingleDetails.js

---
+++ src/components/elements/ProjectSingleDetails.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Badge } from 'react-bootstrap'
+
+const localStyle = {
+    label: {
+        fontWeight: "bold"
+    }
+};
+
+export default function ProjectSingleDetails(props) {
+    return (
+        <div>
+            <div>
+                <span style={localStyle.label}>Platforms : </span>
+                {
+                    props.details.platforms.map(platform => <Badge pill variant="primary">{platform}</Badge> )
+                }
+            </div>
+            <div>
+                <span style={localStyle.label}>Status : </span>
+                { [props.details.status] }
+            </div>
+            <div>
+                <span style={localStyle.label}>Release Date : </span>
+                <span>{ props.details.releaseDate }</span>
+            </div>
+        </div>
+    )
+}

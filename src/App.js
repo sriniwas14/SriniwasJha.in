6b32cfc914e6518cc2e5cffa139a62b073fb4d51
sriniwas14 <sriniwasx@gmail.com>
2020-11-22 00:18:58 +0530

    Implemented Dynamic Header Links

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 import './App.scss';
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useLocation
 } from "react-router-dom";
 
 import Header from './components/elements/Header';
 import Home from './components/Home';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
 import ProjectSingle from './components/ProjectSingle';
 
+
 function App() {
+  const [currentRoute, setcurrentRoute] = useState("/")
+
   return (
     <Router>
       <div className="App">
-        <Header />
+        <Header currentRoute={ currentRoute } setcurrentRoute={ setcurrentRoute } />
 
         <div className="cs_brand" href="#">Sriniwas<b>Jha</b></div>
         <div className="cs_subhead">Coder | Gamer | Geek</div>
 
         <br/>
         <Switch>
-          <Route path="/projects/:projectId">
+          <Route exact path="/projects/:projectId">
               <ProjectSingle />
           </Route>
-          <Route path="/">
+          <Route exact path="/">
               <Home />
+          </Route>
+          <Route exact path="/portfolio">
+              <Portfolio />
+          </Route>
+          <Route exact path="/contact">
+              <Contact />
           </Route>
         </Switch>
 
--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
+import { useLocation, withRouter } from 'react-router-dom'
 
-export default function Header() {
+function Header(props) {
+    var routeItems = [
+        //Route:Label
+        { route: "/", label: "Home" },
+        { route: "/portfolio", label: "Portfolio" },
+        { route: "/contact", label: "Contact" }
+    ]
+
+    const location = useLocation();
+
+    useEffect(() => {
+        console.log(location.pathname)
+        props.setcurrentRoute(location.pathname)
+    }, [location])
+
     return (
         <Navbar variant="dark">
             <Nav className="ml-auto">
-                <Nav.Link active href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Projects</Nav.Link>
-                <Nav.Link href="#pricing">About</Nav.Link>
+                {
+                    routeItems.map(item => <Nav.Link active={props.currentRoute==item.route} href={item.route}>{item.label}</Nav.Link>)
+                }
             </Nav>
         </Navbar>
     )
 }
+export default withRouter(Header)

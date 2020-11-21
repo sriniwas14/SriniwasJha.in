731adf1626bb28e5f09f67aca8ba4433980051d7
sriniwas14 <sriniwasx@gmail.com>
2020-11-22 00:43:02 +0530

    Added Social Media Buttons to the Header

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
+import { AiOutlineGithub, AiOutlineLinkedin, AiOutlineTwitter } from 'react-icons/ai'
 import { useLocation, withRouter } from 'react-router-dom'
 
 function Header(props) {
@@ -19,6 +20,12 @@ function Header(props) {
 
     return (
         <Navbar style={{ backgroundColor: "#0000009c" }} variant="dark">
+            <div>
+
+            <a className="footerLinks" href="https://www.linkedin.com/in/sriniwasj/" target="_black" rel="noreferrer"><AiOutlineLinkedin fontSize={30} /></a>
+                                <a className="footerLinks" href="https://github.com/sriniwas14" target="_black" rel="noreferrer"><AiOutlineGithub fontSize={30} /></a>
+                                <a className="footerLinks" href="https://twitter.com/Sriniwasj" target="_black" rel="noreferrer"><AiOutlineTwitter fontSize={30} /></a>
+            </div>
             <Nav className="ml-auto">
                 {
                     routeItems.map(item => <Nav.Link active={props.currentRoute==item.route} href={item.route}>{item.label}</Nav.Link>)

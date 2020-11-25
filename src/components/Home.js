bb37961c1d744713b05a57930d5d289d3c74ab21
sriniwas14 <sriniwasx@gmail.com>
2020-11-25 11:20:50 +0530

    Changed the intro text

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,7 +19,9 @@ export default function Home() {
             <animated.div style={animPropsAboutMe}>
                 <Row>
                     <Col sm={12}>
-                        <TextSection heading="About Me" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+                        <TextSection heading="About Me">
+                            <p>Hello There! It's me Sriniwas, I've been tinkering with computers since I was a kid and because of that I have earned a deep understanding of it various functions. </p>
+                        </TextSection>
                     </Col>
                 </Row>
             </animated.div>

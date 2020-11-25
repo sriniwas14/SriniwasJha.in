19f536369e76912b0c53322143e22df5e146773d
sriniwas14 <sriniwasx@gmail.com>
2020-11-25 11:58:52 +0530

    Added Chat Component and added Backdrop filter in header

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import { AiOutlineGithub, AiOutlineLinkedin, AiOutlineTwitter } from 'react-icons/ai'
+import { AiOutlineGithub, AiOutlineLinkedin, AiOutlineTwitter, Chat } from 'react-icons/ai'
+import { FaCommentAlt } from 'react-icons/fa'
 import { useLocation, withRouter } from 'react-router-dom'
 
 function Header(props) {
@@ -20,19 +21,20 @@ function Header(props) {
     }, [location])
 
     return (
-        <Navbar style={{ backgroundColor: "#0000009c" }} variant="dark" expand="lg">
+        <Navbar fixed="top" style={{ backgroundColor: "#0000009c", backdropFilter: "blur(9px)" }} variant="dark" expand="lg">
             <div>
 
             <a className="footerLinks" href="https://www.linkedin.com/in/sriniwasj/" target="_black" rel="noreferrer"><AiOutlineLinkedin fontSize={30} /></a>
                                 <a className="footerLinks" href="https://github.com/sriniwas14" target="_black" rel="noreferrer"><AiOutlineGithub fontSize={30} /></a>
                                 <a className="footerLinks" href="https://twitter.com/Sriniwasj" target="_black" rel="noreferrer"><AiOutlineTwitter fontSize={30} /></a>
             </div>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle children={(<FaCommentAlt />)} aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
                     {
                         routeItems.map(item => <Link className={ "nav-link"+(props.currentRoute==item.route ? " active" : "") } to={item.route}>{item.label}</Link>)
                     }
+                    <Link to="/chat"><Button className="chatButton" variant="light" style={{ marginLeft: "12px" }}><FaCommentAlt/> Chat</Button></Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>

9fb43a91453ee834025f9070b6ac7e3487771080
sriniwas14 <sriniwasx@gmail.com>
2021-03-14 22:50:26 +0530

    Added Row and Col in TextSection Component

--- src/components/elements/TextSection.js
+++ src/components/elements/TextSection.js
@@ -1,18 +1,25 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 export default function TextSection(props) {
     return (
         <div className={"cs_Section "+(props.variant==='dark' ? "dark" : "light")}>
-            {
-                props.heading!==undefined ? <h3 className="cs_Headline">{props.heading}</h3> : ""
-            }
-            
-            {
-                props.render!== undefined ? (props.render) : (<p className="cs_Text">{props.text}</p>)
-            }
-            {
-                props.children
-            }
+            <Container>
+                <Row>
+                    <Col sm={12}>
+                {
+                    props.heading!==undefined ? <h3 className="cs_Headline">{props.heading}</h3> : ""
+                }
+                
+                {
+                    props.render!== undefined ? (props.render) : (<p className="cs_Text">{props.text}</p>)
+                }
+                {
+                    props.children
+                }
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }

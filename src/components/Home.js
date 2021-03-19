232c3cbb35267a158b58c4e57bcdf179d30df8fb
sriniwas14 <sriniwasx@gmail.com>
2021-03-19 18:57:03 +0530

    Added My Skills Section

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ import ProjectScroller from './elements/ProjectScroller'
 import { activeProjects } from '../ApiCalls'
 
 import Footer from './elements/Footer'
+import ProgressBar from './elements/ProgressBar'
 
 
 
@@ -21,14 +22,42 @@ export default function Home() {
                 <TextSection className="cs_homeHeader">
                     <Col sm={12} style={{ display: "flex" }}>
                         <div style={{ width: "100%", margin: "auto" }}>
-                            <h1>Hello There! 😀</h1>
+                            <h1>Hello World! 😀</h1>
                             <p>Scroll Down to learn more about me</p>
                         </div>
                     </Col>
                 </TextSection>
             </animated.div>
             <animated.div style={ animPropsAboutMe}>
-                <TextSection variant="dark" heading="Programming" render={(<ProjectScroller projects={activeProjects} />)} />
+                <TextSection variant="dark">
+                    <h2>My Skills</h2>
+                    <p>Here are some of the technologies I have worked with along with how proficient I am with them.</p>
+                    <br />
+                    <Row>
+                        <Col sm={6}>
+                            <h4>Front-End Tech</h4>
+                            <ProgressBar value={85} innerText="ReactJS" />
+                            <ProgressBar value={75} innerText="HTML" />
+                            <ProgressBar value={80} innerText="SCSS" />
+                        </Col>
+                        <Col sm={6}>
+                            <h4>Back-End Tech</h4>
+                            <ProgressBar value={87} innerText="NodeJS" />
+                            <ProgressBar value={60} innerText="Python(Flask)" />
+                            <ProgressBar value={70} innerText="CorePHP" />
+                        </Col>
+                        <Col sm={6}>
+                            <h4>Mobile Tech</h4>
+                            <ProgressBar value={85} innerText="React Native" />
+                            <ProgressBar value={75} innerText="Android (Java)" />
+                        </Col>
+                        <Col sm={6}>
+                            <h4>Databases</h4>
+                            <ProgressBar value={84} innerText="MySQL" />
+                            <ProgressBar value={62} innerText="MongoDB" />
+                        </Col>
+                    </Row>
+                </TextSection>
             </animated.div>
             <animated.div style={ animPropsAboutMe}>
                 <TextSection heading="3D Models" render={(<ProjectScroller projects={activeProjects} />)} />

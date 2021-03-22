64aa67c5c7ec29653185f2b1044f1b2c730ee9f2
sriniwas14 <sriniwasx@gmail.com>
2021-03-22 22:08:40 +0530

    Added Project image and removed build folder from gitignore

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,20 +1,20 @@
 exports.activeProjects = [
-    { projectId: "1", title: "Haskell's Quest", description: "A Roguelike-Metroidvania Game", introduction: "Follow haskell in his quest to destroy Jotnar and bring restore peace to the realm of man.", badges: ["Video Game", "Godot", "GDScript"], src: "http://www.sriniwasjha.in/assets/project_one/project_one_preview.png", details:{ platforms: ["Android", "Windows", "Linux"], status: "In Development", projectUrl: "https://www.github.com", releaseDate: "TBD" } },
-    { projectId: "2", title: "DownMann", description: "A Download Manager/Accelerator for the Gnome Desktop", introduction: "A Download Manager/Accelerator for the Gnome Desktop", badges: ["Desktop App", "Java"], src: "http://www.sriniwasjha.in/assets/project_two/project_two_preview.png", details:{ platforms: ["Linux"], status: "In Development", projectUrl: "https://www.github.com", releaseDate: "TBD" } },
-    { projectId: "3", title: "Elisa", description: "A Kanban Board Made With React and NodeJs", introduction: "A Kanban Board Made With React and NodeJs", badges: ["Web App", "ReactJs", "NodeJs"], src: "http://www.sriniwasjha.in/assets/project_three/project_three_preview.png", details:{ platforms: ["Web"], status: "In Development", projectUrl: "https://www.github.com", releaseDate: "TBD" } },
+    { projectId: "1", title: "Chat App", description: "A Chat Application made with React and Express using Web Sockets", introduction: "Follow haskell in his quest to destroy Jotnar and bring restore peace to the realm of man.", badges: ["Video Game", "Godot", "GDScript"], src: "https://www.sriniwasjha.in/assets/assets/Slides/chatapp_one.jpg", details:{ platforms: ["Android", "Windows", "Linux"], status: "In Development", projectUrl: "https://www.github.com", releaseDate: "TBD" } },
+    { projectId: "2", title: "DownMann", description: "A Download Manager/Accelerator for the Gnome Desktop", introduction: "A Download Manager/Accelerator for the Gnome Desktop", badges: ["Desktop App", "Java"], src: "https://www.sriniwasjha.in/assets/project_two/project_two_preview.png", details:{ platforms: ["Linux"], status: "In Development", projectUrl: "https://www.github.com", releaseDate: "TBD" } },
+    { projectId: "3", title: "Elisa", description: "A Kanban Board Made With React and NodeJs", introduction: "A Kanban Board Made With React and NodeJs", badges: ["Web App", "ReactJs", "NodeJs"], src: "https://www.sriniwasjha.in/assets/project_three/project_three_preview.png", details:{ platforms: ["Web"], status: "In Development", projectUrl: "https://www.github.com", releaseDate: "TBD" } },
 ]
 
 exports.slides = [
     { projectId: "1", slides:[
-        { title: "First", description: "First", image: "http://www.sriniwasjha.in/assets/project_one/project_one_image_one.png" },
-        { title: "Second", description: "Second", image: "http://www.sriniwasjha.in/assets/project_one/project_one_image_two.png" },
-        { title: "Third", description: "Third", image: "http://www.sriniwasjha.in/assets/project_one/project_one_image_three.png" }
+        { title: "First", description: "First", image: "https://www.sriniwasjha.in/assets/project_one/project_one_image_one.png" },
+        { title: "Second", description: "Second", image: "https://www.sriniwasjha.in/assets/project_one/project_one_image_two.png" },
+        { title: "Third", description: "Third", image: "https://www.sriniwasjha.in/assets/project_one/project_one_image_three.png" }
     ] },
     { projectId: "2", slides:[
-        { title: "First", description: "First", image: "http://www.sriniwasjha.in/assets/project_two/project_two_one.png" }
+        { title: "First", description: "First", image: "https://www.sriniwasjha.in/assets/project_two/project_two_one.png" }
     ] },
     { projectId: "3", slides:[
-        { title: "First", description: "First", image: "http://www.sriniwasjha.in/assets/project_three/project_three_one.png" }
+        { title: "First", description: "First", image: "https://www.sriniwasjha.in/assets/project_three/project_three_one.png" }
     ] }
 ]
 

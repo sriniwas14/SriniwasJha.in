849cdcedc98280c3cddf01968f8dec5b37397278
sriniwas14 <sriniwasx@gmail.com>
2021-03-30 22:55:11 +0530

    Added The Contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { useSpring, animated } from 'react-spring';
-
 import TextSection from './elements/TextSection'
 import ProjectScroller from './elements/ProjectScroller'
 import { activeProjects } from '../ApiCalls'
@@ -15,29 +14,32 @@ export default function Contact() {
     const animPropsAboutMe = useSpring({ to:{opacity: 1, transform: 'rotateX(0deg)'}, from: { opacity: 0, transform: 'rotateX(90deg)' }});
 
     return (
-        <Container>
-            <animated.div style={animPropsAboutMe}>
-                <Row>
-                    <Col sm={12}>
-                        <TextSection heading="About Me" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
-                    </Col>
-                </Row>
-            </animated.div>
+        <>
             <animated.div style={ animPropsAboutMe}>
-                <Row>
-                    <Col sm={12}>
-                        <TextSection variant="dark" heading="Programming" render={(<ProjectScroller projects={activeProjects} />)} />
-                    </Col>
-                </Row>
-            </animated.div>
-            <animated.div style={ animPropsAboutMe}>
-                <Row>
-                    <Col sm={12}>
-                        <TextSection heading="3D Models" render={(<ProjectScroller projects={activeProjects} />)} />
-                    </Col>
-                </Row>
+                        <TextSection className="cs_homeHeader" render={(
+                            <div style={{ display: "flex", textAlign: "center"}}>
+                                <div style={{ padding: "7px 10px", cursor: "default", width: "100%" }}>
+                                    <h2 style={{ fontFamily: "oswald" }}>Contact</h2>
+                                    <p>Get in touch with me.</p>
+                                </div>
+                            </div>
+                        )}/>
+                        <TextSection variant="dark" >
+                            <Container>
+                                <form className="customForm">
+                                    <Row>
+                                        <Col sm={{ span: 8, offset: 2 }} >
+                                            <input type="text" name="name" placeholder="Full Name"/>
+                                            <input type="phone" name="phone" placeholder="Phone Number"/>
+                                            <input type="email" name="email" placeholder="E-Mail Address"/>
+                                            <button>Submit</button>
+                                        </Col>
+                                    </Row>
+                                </form>
+                            </Container>
+                        </TextSection>
             </animated.div>
             <Footer />
-        </Container>
+        </>
     )
 }
